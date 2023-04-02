efe68eea22da96e970837f5680704838d2d1f2f0
HfCloud <sxysxygm@gmail.com>
2023-04-02 15:21:29 +0800

    add support for linux/win

--- oidn/__init__.py
+++ oidn/__init__.py
@@ -10,11 +10,11 @@ oidn_py_version = '0.1'
 def __load_lib():
     cur_path = os.path.dirname(__file__)
     if sys.platform == 'linux':
-        raise NotImplementedError("Not implemented for linux")
+        return ctypes.CDLL(os.path.join(cur_path, f"lib.linux.x64/libOpenImageDenoise.{oidn_version}.so"))
     elif sys.platform == 'darwin':
         return ctypes.CDLL(os.path.join(cur_path, f"lib.macos.aarch64/libOpenImageDenoise.{oidn_version}.dylib"))
     elif sys.platform == 'win32':
-        raise NotImplementedError("Not implemented for win32")
+        return ctypes.CDLL(os.path.join(cur_path, f"lib.win.x64/OpenImageDenoise.dll"))
     else:
         raise RuntimeError("Unsupported platform")
 __lib_oidn = __load_lib()
--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import sys 
+if sys.platform == 'darwin':
+    data_files = ["oidn/lib.macos.aarch64/libOpenImageDenoise.1.4.3.dylib"]
+elif sys.platform == 'linux':
+    data_files = ["oidn/lib.linux.x64/libOpenImageDenoise.so.1.4.3", "oidn/lib.linux.x64/libtbb.so.12.5"]
+elif sys.platform == 'win32':
+    data_files = ["oidn/lib.win.x64/OpenImageDenoise.dll", "oidn/lib.win.x64/tbb12.dll"]
 
 setup( 
     name = 'oidn',
@@ -6,9 +13,11 @@ setup(
     author = 'HfCloud',
     author_email = 'sxysxygm@gmail.com',
     description = 'A simple python binding for Intel OIDN',
-    packages=find_packages(),
+    url="https://github.com/sxysxy/OIDN-python",
+    package_dir='oidn',
     install_requirs=[
         'numpy >= 1.12.0',
         'pillow'
-    ]
+    ],
+    data_files=data_files
 )
